bc0c5161ffb0f596ca27b4c9e9cd6b86a230ce03
Divyayush Verma <122596172+divyayush-v-20@users.noreply.github.com>
2025-03-29 15:11:22 +0530

    Create index.js

---
+++ Backend/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors'); // Import CORS
+const adminRouter = require("./routes/admin");
+const userRouter = require("./routes/user");
+const courseRouter = require("./routes/courses")
+const connectDB = require("./db/index")
+const app = express();
+const cookieParser = require('cookie-parser');
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", 
+    credentials: true 
+  })
+);
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+
+connectDB()
+app.use("/admin", adminRouter);
+app.use("/user", userRouter);
+app.use("/courses" , courseRouter);
+
+const PORT = 3000; 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
